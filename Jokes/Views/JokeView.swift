894b621b71c70ced5e340d56ce70936d655bba55
Justin <jwei@lcs.on.ca>
2023-04-14 11:25:23 -0400

    button now shows punchline

---
+++ Jokes/Views/JokeView.swift
@@ -0,0 +1,51 @@
+//
+//  JokesView.swift
+//  Jokes
+//
+//  Created by Justin Zack Wei on 2023-04-14.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    //MARK: STORED PROPERTIES
+    
+    @State var punchlineOpacity = 0.0
+    
+    //MARK: COMPUTED PROPERTIES
+    
+    
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text("You see, mountains are NOT just funny!!")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                
+                
+                Button(action: {
+                    punchlineOpacity = 1.0
+                },label:{
+                    Image(systemName: "arrow.down.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width:40)
+                        .tint(.black)
+                })
+                Text("They are hill areas")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .opacity(punchlineOpacity)
+            }
+            .navigationTitle("Random Jokes")
+        }
+    }
+    
+    struct JokeView_Previews: PreviewProvider {
+        static var previews: some View {
+            
+            JokeView()
+            
+        }
+    }
+}
