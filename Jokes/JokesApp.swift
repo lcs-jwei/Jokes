32d85db4bdf1307872e4a456af4dded7fc07c620
Justin <jwei@lcs.on.ca>
2023-04-14 11:12:23 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Justin Zack Wei on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
